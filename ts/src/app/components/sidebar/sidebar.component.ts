a127674a5ed3ffbe00fa401d4fe4f64501e6c220
Lucas Cester <lucascester@hotmail.com>
2024-05-04 08:27:00 -0300

    Update: Last polish

--- ts/src/app/components/sidebar/sidebar.component.ts
+++ ts/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements AfterContentInit {
 
-  constructor() { }
+  oldOption?: HTMLElement;
+  newOption?: HTMLElement;
 
-  ngOnInit() {
+  public ngAfterContentInit(): void {
+    var oldElement = document.getElementById("item-5");
+    if(oldElement == undefined || oldElement == null)
+      return;
+
+    this.oldOption = oldElement;
+    this.oldOption.classList.add("ativo");
+    this.oldOption.firstElementChild?.classList.add("ativo");
   }
 
+  public highlightSelectedOption(elementId: string): void{
+    var newElement = document.getElementById(elementId);
+    if(newElement == undefined || newElement == null)
+      return;
+
+    if(this.oldOption == null || this.oldOption == undefined)
+    {
+      this.oldOption = newElement;
+      this.oldOption.classList.add('ativo');
+      this.oldOption.firstElementChild?.classList.add("ativo");
+    }
+    else
+    {
+      this.newOption = newElement;
+      this.changeSelectedClasses();
+    }
+  }
+
+  public changeSelectedClasses(): void{
+    if(this.oldOption == null || this.oldOption == undefined && 
+      this.newOption == null || this.newOption == undefined)
+      return;
+
+    this.oldOption.classList.remove("ativo");
+    this.oldOption.firstElementChild?.classList.remove("ativo");
+
+    this.newOption.classList.add('ativo');
+    this.newOption.firstElementChild?.classList.add("ativo");
+
+    this.oldOption = this.newOption;
+  }
 }
